e605b93849bf59128f631a2a24f1c681940b2e37
Mahyuddin Susanto <udienz@gmail.com>
2016-08-12 06:20:41 +0700

    WORDRESS: Add fpm configurration
      - gen-env: Also generate WORDPRESS_DB_* environment
      - WIP: Create fpm for wordpress
    
    Signed-off-by: Mahyuddin Susanto <udienz@gmail.com>

--- wordpress/gen-env.sh
+++ wordpress/gen-env.sh
@@ -11,10 +11,17 @@ if [ -f common.env ]; then
     mv common.env common.env.orig
 fi
 
-
+get_env ()
+{
+    grep $1 common.env | sed -e 's/=/ /g' | awk {'print$2'}
+}
 ENV="MYSQL_ROOT_PASSWORD MYSQL_DATABASE MYSQL_PASSWORD MYSQL_USER"
 
 for a in $ENV
 do
     echo "$a=$(gen_random)" >> common.env
 done
+
+echo "WORDPRESS_DB_PASSWORD=$(get_env MYSQL_PASSWORD)" >> common.env
+echo "WORDPRESS_DB_USER=$(get_env MYSQL_USER)" >> common.env
+echo "WORDPRESS_DB_NAME=$(get_env MYSQL_DATABASE)" >> common.env
